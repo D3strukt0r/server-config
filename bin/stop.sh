f2cba1e91332a67275a2c309b9a1dce1ff00cf1d
D3strukt0r <dev@d3strukt0r.me>
2024-04-30 05:59:40 +0000

    Add scripts for easier start and shutdown

---
+++ bin/stop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e -u -o pipefail
+
+# Script dir (https://stackoverflow.com/a/246128/4156752)
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECTS_DIR=$(realpath "$SCRIPT_DIR/../prod")
+
+# start all projects except "fluentd"
+PROJECTS=$(ls $PROJECTS_DIR | sed 's/fluentd//')
+for PROJECT in $PROJECTS
+do
+    cd $PROJECTS_DIR/$PROJECT
+    docker compose down
+done
+
+# "fluentd" must be stopped after all projects are shut down
+cd $PROJECTS_DIR/fluentd
+docker compose down
