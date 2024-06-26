cd7b8e16937cf304249d17ee025d4411aae66cea
D3strukt0r <dev@d3strukt0r.me>
2024-06-26 22:17:20 +0000

    Add start/stop scripts for environments

--- bin/docker-services-start.sh
+++ bin/docker-services-start.sh
@@ -6,4 +6,37 @@ if [ -f /etc/default/docker-services ]; then
   . /etc/default/docker-services
 fi
 
-ENVIRONMENT_DIR="$REPO_DIR/$SERVER_ENVIRONMENT"
+ENVIRONMENT_DIR=$(realpath "$REPO_DIR/$SERVER_ENVIRONMENT")
+
+# https://stackoverflow.com/questions/75101230/find-for-yml-and-yaml-files-on-bash-find
+# https://stackoverflow.com/questions/23356779/how-can-i-store-the-find-command-results-as-an-array-in-bash/54561526#54561526
+readarray -d '' docker_compose_paths < <(find "$ENVIRONMENT_DIR" -regextype egrep -regex '.*ya?ml$' -print0)
+
+# https://stackoverflow.com/questions/29225972/validating-docker-compose-yml-file
+# https://stackoverflow.com/questions/3578584/bash-how-to-delete-elements-from-an-array-based-on-a-pattern
+FLUENTD_SERVICE_PATH=""
+for i in "${!docker_compose_paths[@]}"; do
+    docker_compose_path="${docker_compose_paths[$i]}"
+    if ! docker compose -f "$docker_compose_path" config > /dev/null 2>&1; then
+        # filter out all invalid yaml files
+        unset -v 'docker_compose_paths[$i]'
+    elif [[ "$docker_compose_path" == *"fluentd"* ]]; then
+        # if the path has anything with fluentd, remove it and save separately
+        FLUENTD_SERVICE_PATH="$docker_compose_path"
+        unset -v 'docker_compose_paths[$i]'
+    fi
+done
+# append at beginning of list if fluentd service is found
+if [ -n "$FLUENTD_SERVICE_PATH" ]; then
+    docker_compose_paths=("$FLUENTD_SERVICE_PATH" "${docker_compose_paths[@]}")
+fi
+
+for docker_compose_path in "${docker_compose_paths[@]}"; do
+    docker_compose_dir=$(dirname "$docker_compose_path")
+    docker_compose_file=$(basename "$docker_compose_path")
+    (
+        echo "Starting $docker_compose_file in $docker_compose_dir"
+        cd "$docker_compose_dir"
+        docker compose -f "$docker_compose_file" up -d
+    )
+done
--- bin/docker-services-stop.sh
+++ bin/docker-services-stop.sh
@@ -6,22 +6,37 @@ if [ -f /etc/default/docker-services ]; then
   . /etc/default/docker-services
 fi
 
-ENVIRONMENT_DIR="$REPO_DIR/$SERVER_ENVIRONMENT"
+ENVIRONMENT_DIR=$(realpath "$REPO_DIR/$SERVER_ENVIRONMENT")
 
 # https://stackoverflow.com/questions/75101230/find-for-yml-and-yaml-files-on-bash-find
 # https://stackoverflow.com/questions/23356779/how-can-i-store-the-find-command-results-as-an-array-in-bash/54561526#54561526
-readarray -d '' DOCKER_COMPOSE_PATHS < <(find "$ENVIRONMENT_DIR" -regextype egrep -regex '.*ya?ml$' -print0)
-
-#echo "${DOCKER_COMPOSE_PATHS[@]}"
+readarray -d '' docker_compose_paths < <(find "$ENVIRONMENT_DIR" -regextype egrep -regex '.*ya?ml$' -print0)
 
 # https://stackoverflow.com/questions/29225972/validating-docker-compose-yml-file
 # https://stackoverflow.com/questions/3578584/bash-how-to-delete-elements-from-an-array-based-on-a-pattern
-# filter out all invalid yaml files
-for i in "${!DOCKER_COMPOSE_PATHS[@]}"; do
-    dc_path="${DOCKER_COMPOSE_PATHS[$i]}"
-    if ! docker compose -f "$dc_path" config > /dev/null 2>&1; then
-        unset -v 'DOCKER_COMPOSE_PATHS[$i]'
+FLUENTD_SERVICE_PATH=""
+for i in "${!docker_compose_paths[@]}"; do
+    docker_compose_path="${docker_compose_paths[$i]}"
+    if ! docker compose -f "$docker_compose_path" config > /dev/null 2>&1; then
+        # filter out all invalid yaml files
+        unset -v 'docker_compose_paths[$i]'
+    elif [[ "$docker_compose_path" == *"fluentd"* ]]; then
+        # if the path has anything with fluentd, remove it and save separately
+        FLUENTD_SERVICE_PATH="$docker_compose_path"
+        unset -v 'docker_compose_paths[$i]'
     fi
 done
+# append at end of list if fluentd service is found
+if [ -n "$FLUENTD_SERVICE_PATH" ]; then
+    docker_compose_paths+=("$FLUENTD_SERVICE_PATH")
+fi
 
-#echo "${DOCKER_COMPOSE_PATHS[@]}"
+for docker_compose_path in "${docker_compose_paths[@]}"; do
+    docker_compose_dir=$(dirname "$docker_compose_path")
+    docker_compose_file=$(basename "$docker_compose_path")
+    (
+        echo "Stopping $docker_compose_file in $docker_compose_dir"
+        cd "$docker_compose_dir"
+        docker compose -f "$docker_compose_file" down
+    )
+done
